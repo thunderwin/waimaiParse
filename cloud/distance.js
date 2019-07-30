ea76cb28b4376ab7c5c2a5bcdc599f98a88fa5d6
BigLazybones <1933323175@qq.com>
2019-07-30 08:59:33 +0800

    get menulist

--- cloud/distance.js
+++ cloud/distance.js
@@ -1,4 +1,4 @@
-var Parse = require('parse/node')
+﻿var Parse = require('parse/node')
 const Desk = Parse.Object.extend('Desk')
 
 
@@ -74,7 +74,9 @@ module.exports = {
         const query = new Parse.Query(ShopList);
         query.near("location", point);
         const placesObjects = await query.find();
+        console.log("placesObjects",placesObjects)
         let temp = placesObjects.map(x=>x._toFullJSON())
+        console.log("temp",temp)
         let afterShopList = []
         for(let key in temp){
             //console.log(temp[key].shopName)
@@ -144,11 +146,98 @@ module.exports = {
     leadToStoreHome: async req =>{
         let ShopList = Parse.Object.extend("ShopList")
         const query = new Parse.Query("ShopList")
-        const shopList = await query.find()
-        // let temp = shopList.map(x=>x._toFullJSON())
-        //console.log("***************", temp)
+        const shopList = await query.find();
+        let temp = shopList.map(x=>x._toFullJSON())
+        let tempO = temp[2]
+        // console.log(tempO.objectId)
+        //console.log(tempO)
+
+
+        let r = await new Parse.Query('mapShopCatgItem').find()
+
+        const q = new Parse.Query('mapShopCatgItem')
+        let qq = await q.include("itemPointer").find()
+
+
+        console.log("************",qq)
+        //console.log(r.map(x=>x._toFullJSON())) 
+        let rr = r.map(x=>x._toFullJSON())
+        var getCategoryPointer = []
+        for (let key in rr){
+            //console.log(rr[key].shopPointer)
+            if(rr[key].shopPointer.objectId == tempO.objectId){
+                //获得ParseObject { _objCount: 26, className: 'mapShopCatgItem', id: 'i4A5OXlpN1' }
+                //var getColumn = await new Parse.Query('mapShopCatgItem').equalTo('ShopPointer',rr[key].ShopPointer).find()
+                //获得中间表中符合的ShopPointer那些行的CategoryPointer的字符串
+                getCategoryPointer.push(rr[key].categoryPointer.objectId)
+            }
+        }
+        getCategoryPointer = new Set(getCategoryPointer)
+        console.log("cat",getCategoryPointer)
+        for(let x of getCategoryPointer){
+            console.log(x)
+        }
+
+        let shopMenuList = []
+        let catr = await new Parse.Query('Categories').find()
+        let catrr = catr.map(x=>x._toFullJSON())
+        let itemr = await new Parse.Query('Items').find()
+        let itemrr = itemr.map(x=>x._toFullJSON())
+        for(let x of getCategoryPointer){ 
+            for (let key in catrr){
+                if(catrr[key].objectId == x && catrr[key].catgStatus == 1){
+                    let shopMenu = {}
+                    shopMenu.id =  catrr[key].catgPosition
+                    shopMenu.name = catrr[key].categoryName
+                    // for(let index in itemrr){
+                    //     if(itemrr[index].itToCatPointer.objectId == x && itemrr[index].itemStatus == 1){
+                    //         let items = []
+                    //         items.itemId = itemrr[index].itemPosition
+                    //         items.itemName = itemrr[index].itemName
+                    //         items.itemPrice = itemrr[index].itemPrice
+                    //         console.log("----------",items)
+                    //         shopMenu.items = items
+                    //     }
+                    // }
+                    shopMenuList.push(shopMenu)
+                }
+            }
+        }
+        var by = function (prop) {
+            return function(obj1, obj2) {
+                var val1 = obj1[prop];
+                var val2 = obj2[prop];
+                if (val1 < val2) {
+                    return -1;
+                } else if (val1 > val2) {
+                    return 1;
+                } else {
+                    return 0;
+                }            
+            }
+        }
+        shopMenuList.sort(by("id"));
+        console.log("+++++++++++",shopMenuList)
+
+
+        // var getCategoryName = []
+        // for (let key in getCategoryPointer){
+        //     console.log(getCategoryPointer[key])
+        //     var CategoryName = await new Parse.Query("Categories").equalTo('objectId',getCategoryPointer[key]).find()
+        //     getCategoryName.push(CategoryName)
+        //     console.log(getCategoryName)
+        // }
+        // console.log("catname",getCategoryName)
+        
+        //console.log(test[0].ItemPointer)
+        console.log("---------------",temp[2]._toFullJSON())
+        // console.log(shopList[0]._toFullJSON())
+        return(shopList.map(x=>x._toFullJSON()))
+
+        
         for(let key in shopList){
-            console.log("++++++++++",JSON.stringify(shopList[6].get("ItemPointer")))
+            console.log(Json.stringify(shopList[6]))
+            console.log("++++++++++",shopList[6].get("shopToMap"))
             let tempPointer =shopList[6].get("ItemPointer")
             console.log(JSON.stringify(tempPointer.get("bala")))
         }
@@ -156,10 +245,10 @@ module.exports = {
         var relation = user.relation("likes");
         relation.add(post);
         user.save();
-    },
+    }, 
 
 
-
+    
 
 
 
